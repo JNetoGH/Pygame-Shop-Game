591461fe0f4e39b2d581d8698c3742d337602655
JNetoGH <joaomendes15@gmail.com>
2022-12-07 07:22:17 +0000

    it doesn't work with uncentralized collider and therefore multiple collider, but I made a acurracy improvement to the conllisions rounding the numbers of the rectangles postion

--- game/_2components/collider/collider.py
+++ game/_2components/collider/collider.py
@@ -20,23 +20,24 @@ class Collider(Component):
         self.collider_rect: pygame.Rect = pygame.Rect(0, 0, 0, 0)
 
         # sets the collider_rect default shape and position
-        self.collider_rect.centerx = self.game_object_owner.transform.world_position.x + self.offset_from_game_object_x
-        self.collider_rect.centery = self.game_object_owner.transform.world_position.y + self.offset_from_game_object_y
+        # pygame is stupid, it only uses ints to represent rectanclge what truncates the float from 1.9 to 1 for example
+        # making pretty bad collision, so i am rounding what makes 1.9 => 2 and 1.2 => 1, what is a bit better
         self.collider_rect.width = self.width
         self.collider_rect.height = self.height
+        self.collider_rect.centerx = round(self.game_object_owner.transform.world_position.x + self.offset_from_game_object_x)
+        self.collider_rect.centery = round(self.game_object_owner.transform.world_position.y + self.offset_from_game_object_y)
 
         # for collision checking
         self._is_colliding = False
 
     def check_collision(self, another_collider):
-
         pass
 
     def component_update(self):
-        self.collider_rect.centerx = self.game_object_owner.transform.world_position.x + self.offset_from_game_object_x
-        self.collider_rect.centery = self.game_object_owner.transform.world_position.y + self.offset_from_game_object_y
         self.collider_rect.width = self.width
         self.collider_rect.height = self.height
+        self.collider_rect.centerx = self.game_object_owner.transform.world_position.x + self.offset_from_game_object_x
+        self.collider_rect.centery = self.game_object_owner.transform.world_position.y + self.offset_from_game_object_y
 
     def get_inspector_debugging_status(self) -> str:
         return "Component(Collider)\n"
